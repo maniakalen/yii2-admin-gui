3af7850265b6357c3bb0cac2f274e44e81fa83c3
Peter Georgiev <pesho10@yahoo.com>
2019-11-06 13:04:29 +0100

    add title and params to some actions

--- src/maniakalen/admingui/actions/Action.php
+++ src/maniakalen/admingui/actions/Action.php
@@ -10,12 +10,14 @@ class Action extends \yii\base\Action
 {
     const EVENT_BEFORE_RUN = 'before_run';
     const EVENT_AFTER_RUN = 'after_run';
+
     /**
      * This method is called right before `run()` is executed.
      * You may override this method to do preparation work for the action run.
      * If the method returns false, it will cancel the action.
      *
      * @return bool whether to run the action.
+     * @throws \yii\base\InvalidConfigException
      */
     protected function beforeRun()
     {
